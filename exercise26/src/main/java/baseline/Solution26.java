49997c37f9b72eed86aba892547db2c29d3cbe69
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 15:40:30 -0400

    Solution 26 initial commit with Psuedocode

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 26
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+/* create PaymentCalculator with given formula rounding to nearest integer
+ * get input for balance, APR in percentage form, and monthly payment
+ * use calculateMonthsUntilPaidOff to print resulting months remaining
+ */
+
+package baseline;
+
+public class Solution26 {
+
+}
