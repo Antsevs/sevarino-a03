46f4e9b4a45600e2a66fc8b10683ea122a1bbd44
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 18:02:54 -0400

    Solution28 with full code

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -10,5 +10,20 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
+    public static void main(String[] args) {
+        //establish main variable and scanner
+        int sum = 0;
+        Scanner scnr = new Scanner(System.in);
+        //loop 5 times
+        for (int n = 0; n < 5; n++) {
+            System.out.println("Enter a number: ");
+            //add the scanner input to the sum each iteration
+            sum += scnr.nextInt();
+        }
+        //print resulting sum
+        System.out.println("The total is " + sum + ".");
+    }
 }
