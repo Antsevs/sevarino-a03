faf5d8fe4bc05648ffd361c66c946fa8b243057d
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 16:48:09 -0400

    Solution 27 initial commit with Psuedocode

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+
+/* get user first and last name and restrict to only letters
+ * in validateInput add minimum 2 letter restriction
+ * get user zip code
+ * in validateInput restrict zip code to only numbers and exactly 5 numbers
+ * get user employee ID
+ * in validateInput restrict as per instructions
+ * print message showing any errors in these inputs
+ */
+
+package baseline;
+
+public class Solution27 {
+}
