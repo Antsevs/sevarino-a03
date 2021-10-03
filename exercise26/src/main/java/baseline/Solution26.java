f014f9e3547ea80e1d292e2a76c24d3f4087ed33
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 16:02:58 -0400

    Solution 26 with full code

--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -10,6 +10,36 @@
 
 package baseline;
 
-public class Solution26 {
+import java.util.Scanner;
 
+public class Solution26 {
+    public class PaymentCalculator{
+        public static int calculateMonthsUntilPaidOff(double balance, double APR, double monthly){
+            APR = APR/365;
+            int formula = (int) Math.ceil((-1 / 30.0) * Math.log(1 + balance / monthly
+                    * (1 - Math.pow(1 + APR, 30))) / Math.log(1 + APR));
+            return formula;
+        }
+    }
+
+    public static void main(String[] args) {
+        int monthsRem;
+
+        Scanner scnr = new Scanner(System.in);
+
+        double balance, APR, monthly;
+
+        System.out.println("What is your balance? ");
+        balance = scnr.nextDouble();
+
+        System.out.println("What is the APR on the card (as a percent)? ");
+        APR = scnr.nextDouble();
+        APR = APR / 100;
+
+        System.out.println("What is the monthly payment you can make? ");
+        monthly = scnr.nextDouble();
+
+        monthsRem = PaymentCalculator.calculateMonthsUntilPaidOff(balance, APR, monthly);
+        System.out.println("It will take you " + monthsRem + " months to pay off this card.");
+    }
 }
