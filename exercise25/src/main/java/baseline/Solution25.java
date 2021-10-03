4ac75ef6984e6ad2a43851cf987035bd6b218dc0
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 14:37:22 -0400

    exercise 25 initial commit with psuedo code

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,19 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution 25
+ * Copyright 2021 Anthony Sevarino
+ */
+
+/* setup boolean for each possible input (special character, digit, letters)
+ * set an array for all viable special characters
+ * set the password to an array and check each input for digit, special character, and letter
+ * check for length of password and assign proper strength checking with letters digits and sp characters
+ * scan for input and use passwordValidator function
+ * print resulting strength
+ */
+
+
+package baseline;
+
+public class Solution25 {
+    
+}
