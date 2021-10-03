787a8bd062115e02a491a8af86cd899047d2d877
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 10:19:37 -0400

    exercise 24 initial psuedocode commit

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 24
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+/* setup boolean function for true
+ * if the two strings are not of equal length, boolean is false
+ * convert both strings to fully lowercase
+ * put strings in an array
+ * sort the array alphabetically
+ * if the two arrays are equal, then the boolean remains true
+ * get first and second string input from user
+ * use isAnagram function to check for anagram presence
+ * print resulting outcome
+ */
+
+package baseline;
+
+public class Solution24 {
+
+}
