f21f4ca2d2e13b86a4850c92f4be33b8de4bb7e9
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 17:42:29 -0400

    Solution28 initial commit with Psuedocode

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,14 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution 28
+ * Copyright 2021 Anthony Sevarino
+ */
+
+/* Establish a loop with two counters, one to manage number of inputs, and one to contain user inputs
+ * Prompt user for first input and store resulting number
+ * after 5 iterations, add the store numbers and print the result
+ */
+
+package baseline;
+
+public class Solution28 {
+}
