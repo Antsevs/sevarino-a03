eaedc181fb8da21a626a365d1129d48ce3349961
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 22:57:52 -0400

    Solution 33 initial commit with Psuedocode

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 33
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+
+/* create an array with the potential responses
+ * add randomizer and apply it to the array
+ * scan for user question
+ * print response
+ */
+package baseline;
+
+public class Solution33 {
+}
