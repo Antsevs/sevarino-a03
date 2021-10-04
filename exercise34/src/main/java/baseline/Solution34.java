c47651c696f348dad0d82d06ee898673f1d0e72a
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 23:32:44 -0400

    Solution 34 initial commit with Psuedocode

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 34
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+/* Create an array list of employee names and list them to user
+ * prompt user for which name they would like to remove
+ * remove user input from list directly
+ * print resulting list
+ */
+
+package baseline;
+
+public class Solution34 {
+}
