1a7636792cc825dec53f244ab763e9e0d07912f6
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 21:23:13 -0400

    Solution 31 initial commit with Psuedocode

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,16 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution 31
+ * Copyright 2021 Anthony Sevarino
+ */
+
+/* get the resting heart rate from the user (continue until proper input)
+ * get age from user (continue until proper input)
+ * create a function that will determine if the above two inputs are integers
+ * use the calculation given to find target heart rate for each intensity level
+ */
+
+package baseline;
+
+public class Solution31 {
+
+}
