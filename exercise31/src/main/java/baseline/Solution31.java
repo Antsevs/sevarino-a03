3bcd8067f02a0af17139d3e2d184d3e2cc8f2ad5
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 21:36:12 -0400

    Solution 31 with full code

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -11,6 +11,55 @@
 
 package baseline;
 
-public class Solution31 {
+import java.util.Scanner;
 
+public class Solution31 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int rest, age, intensity, rate;
+        //keep asking user until they stop being rude and give a proper answer
+        while(true) {
+            System.out.println("Resting Pulse: ");
+            String strng1 = scnr.nextLine();
+            //use separate function to check if the input is a number
+            if (numCheck(strng1)) {
+                rest = Integer.parseInt(strng1);
+                break;
+            } else {
+                System.out.println("Please enter valid pulse.");
+            }
+        }
+        //do the same thing I said above but with the age now
+        while(true){
+            System.out.println("Age: ");
+            String strng1 = scnr.nextLine();
+            //this is where the separate function comes in again
+            if(numCheck(strng1)){
+                age = Integer.parseInt(strng1);
+                break;
+            } else {
+                System.out.println("Please enter valid age.");
+            }
+        }
+        //print everything with proper table formatting
+        System.out.println("Intensity \t | Rate");
+        System.out.println("----------------------");
+        for(intensity = 55; intensity <= 95; intensity +=5){
+            rate = ((220 - age - rest) * intensity / 100) + rest;
+
+            System.out.println(intensity + "%\t\t\t | " + rate + " bpm");
+        }
+    }
+
+    /*this is where that separate function that checks if the input is a
+     * number or not is hiding
+     */
+    public static boolean numCheck(String strng1){
+        try{
+            Integer.parseInt(strng1);
+        } catch(Exception e){
+            return false;
+        }
+        return true;
+    }
 }
