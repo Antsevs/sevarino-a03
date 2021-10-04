99cb923ed4bc902d04c0cbe00ac0b9e665d9e7d1
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 21:10:59 -0400

    Solution 30 initial commit with Psuedocode

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,16 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solution 30
+ * Copyright 2021 Anthony Sevarino
+ */
+
+/* begin initial loop that counts from 1 up to 12
+ * nest additional loop with different variable that counts 1 to 12
+ * multiply these two variables through every instance
+ * align numbers in table without using \t including new line each 12
+ */
+
+package baseline;
+
+public class Solution30 {
+    
+}
