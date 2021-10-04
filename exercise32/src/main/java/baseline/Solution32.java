112a95cb77686c6e1578c478cf707c954e86a1ee
Anthony Sevarino <sevarinoanthony@gmail.com>
2021-10-03 21:44:11 -0400

    Solution 32 initial commit with Psuedocode

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution 32
+ *  Copyright 2021 Anthony Sevarino
+ */
+
+/* create a random number generator for each difficulty with proper ranges
+ * after user inputs difficulty level, computer picks random number
+ * create loop that will continually ask user for number until it matches
+ * create counter to count how many attempts it takes user to successfully guess
+ * tell user if their guess is below or above the selected number
+ * print number of attempts taken
+ * prompt user to play again, if Y then resent counter to 0
+ */
+
+package baseline;
+
+public class Solution32 {
+}
